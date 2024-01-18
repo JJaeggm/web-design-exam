69b26102ebbdd79b07cef57faae051b14ee470ce
JJaeggm <raptor1130@gmail.com>
2024-01-18 17:09:06 +0900

    20240118

--- E-4/js/main.js
+++ E-4/js/main.js
@@ -1,9 +1,30 @@
-$(function() {
+$(function () {
   // 메뉴
-  $(".nav > ul > li").mouseenter(function() {
-    $(this).find("ul").stop().fadeIn()/* .show() */;
+  $('.nav > ul > li').mouseenter(function () {
+    $(this).find('ul').stop().fadeIn() /* .show() */;
   });
-  $(".nav > ul > li").mouseleave(function() {
-    $(".sub_menu").stop().fadeOut()/* .hide() */;
+  $('.nav > ul > li').mouseleave(function () {
+    $('.sub_menu').stop().fadeOut() /* .hide() */;
   });
+
+  // 슬라이드
+  // setInterval(function () {
+  //   $('.slide ul').animate({ left: '-100%' }, 200, function () {
+  //     $('.slide ul').appendTo($('.slide ul li').eq(0));
+  //     $('.slide ul').css('left', 0);
+  //   });
+  // }, 3000);
+
+  var i = 0;
+
+  function slide() {
+    if (i < 2) {
+      i++;
+    } else {
+      i = 0;
+    }
+
+    $('.slide ul').animate({ left: -100 * i + '%' }, 200);
+  }
+  setInterval(slide, 1000);
 });
